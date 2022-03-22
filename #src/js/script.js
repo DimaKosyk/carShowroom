e997102ac63c272bb5ffe8effd928bf4becfbffe
DimaKosyk <dimakosyk93@gmail.com>
2022-03-22 21:56:12 +0200

    finished section blog

--- #src/js/script.js
+++ #src/js/script.js
@@ -2,3 +2,15 @@
 @@include('./rangeSlider.js')
 @@include('./formStyler.js')
 @@include('./rateyo.js')
+
+
+$(function () {
+  $('.blog-tabs__top-item').on('click', function (e) {
+    e.preventDefault();
+    $('.blog-tabs__top-item').removeClass('blog-tabs__top-item--active');
+    $(this).addClass('blog-tabs__top-item--active');
+
+    $('.blog-tabs__content-item').removeClass('blog-tabs__content-item--active');
+    $($(this).attr('href')).addClass('blog-tabs__content-item--active');
+  });
+});
